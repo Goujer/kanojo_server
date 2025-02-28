87352a9be6b4579d2ef82b3c055107459fd4b958
Goujer <legojr33@gmail.com>
2025-02-27 18:45:04 -0600

    Dialog corrections
    Better handle dialog bugs.
    Corrected some grammar.
    Document the functions.
    Make some dialog more appropriate to certain situations.

--- reactionword.py
+++ reactionword.py
@@ -14,10 +14,29 @@ class ReactionwordManager(object):
 		tmp = json.load(open(reactionword_file, encoding='utf-8'))
 		self._items = tmp.get('reactionword')
 
-	def reactionword_json(self, a, pod):
-		itms = [x for x in self._items if a in x.get('a') and ('pod' not in x or pod in x.get('pod'))]
+	#TODO Add Reaction Words from recovered media
+
+	'''
+		a - action param
+			1 - gift to kanojo
+			2 - extended gift
+			3 - date
+			4 - extended date (not use)
+			10,11,12 - main touch action
+			20,21 - main touch by stamina action 
+		pod - part of day param
+			0 - night
+			1 - morning
+			2 - day
+			3 - evening
+	'''
+	def reactionword_json(self, actions: list[int], pod: int):
+		itms = [x for x in self._items for a in actions if a in x.get('a') and ('pod' not in x or pod in x.get('pod'))]
 		if len(itms):
 			itm = random.choice(itms)
-			return itm.get('data')
+			val = itm.get('data')
 		else:
-			return json.dumps(["…",])
+			val = ["Umm, the server messed up", "I don't know what to say, sorry", "Ask Goujer for help please."]
+
+		return {"text": val,
+				"btn_text": "Next"}
